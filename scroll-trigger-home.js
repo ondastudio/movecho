6f2237978a01e2ec83617cb6d10fd0c813a55f86
Onda <75018767+ondastudio@users.noreply.github.com>
2023-11-16 18:06:22 +0000

    Create scroll-trigger-home.js

---
+++ scroll-trigger-home.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hero = document.querySelector(".intro-video");
+  let hInside = { v: true };
+  const vid1 = document.querySelector(".vid1");
+  let vInside1 = { v: false };
+  const vid2 = document.querySelector(".vid2");
+  let vInside2 = { v: false };
+  const vid3 = document.querySelector(".vid3");
+  let vInside3 = { v: false };
+  const vid4 = document.querySelector(".vid4");
+  let vInside4 = { v: false };
+
+  gsap.registerPlugin(ScrollTrigger);
+  ScrollTrigger.create({
+    start: 0,
+    end: "max",
+    onUpdate: () => {
+      toggleVideos(hero, hInside);
+      toggleVideos(vid1, vInside1);
+      toggleVideos(vid2, vInside2);
+      toggleVideos(vid3, vInside3);
+      toggleVideos(vid4, vInside4);
+    }
+  });
+
+  function toggleVideos(vid, inside) {
+    if (ScrollTrigger.isInViewport(vid, 0) && !inside.v) {
+      vid.play();
+      inside.v = true;
+    } else if (!ScrollTrigger.isInViewport(vid, 0) && inside.v) {
+      vid.pause();
+      inside.v = false;
+    }
+  }
+});
