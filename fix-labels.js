5251fa537cdd7b06442c535052a978772e008e68
Onda <75018767+ondastudio@users.noreply.github.com>
2023-11-24 17:37:30 +0000

    Create fix-labels.js

---
+++ fix-labels.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+  const names = document.querySelectorAll(".home_proj-name_wrap");
+
+  names.forEach((name) => {
+    name.style.width = getTextWidth(name.querySelector("p")) + "px";
+  });
+
+  function getTextWidth(el) {
+    const text = el;
+    const width = Math.ceil(text.getBoundingClientRect().width);
+    return width;
+  }
+});
