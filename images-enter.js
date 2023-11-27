c7f497592f8f4eb50bd76e64992a063d237f7043
Onda <75018767+ondastudio@users.noreply.github.com>
2023-11-27 17:46:18 +0000

    Create images-enter.js

---
+++ images-enter.js
@@ -0,0 +1,90 @@
+// pageload
+window.addEventListener("load", () => {
+  const vids = document.querySelectorAll("video");
+  const imgs = document.querySelectorAll(".img-anim");
+
+  vids.forEach((v) => {
+    if (!v.classList.contains("intro-video")) createSwipeVid(v);
+  });
+  imgs.forEach((i) => {
+    createSwipeImg(i);
+  });
+
+  function createSwipeVid(el) {
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: el.parentElement,
+        start: "top 90%",
+        end: "top 90%"
+      }
+    });
+    let tl2 = gsap.timeline({
+      scrollTrigger: {
+        trigger: el.parentElement,
+        start: "top 90%",
+        end: "top 90%"
+      }
+    });
+    tl.fromTo(
+      el.parentElement,
+      {
+        clipPath: "polygon(10% 10%, 90% 10%, 90% 90%, 10% 90%)"
+      },
+      {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        duration: 2,
+        ease: "power3.out"
+      }
+    );
+    tl2.fromTo(
+      el,
+      {
+        scale: 1.5
+      },
+      {
+        scale: 1,
+        duration: 2,
+        ease: "power3.out"
+      }
+    );
+  }
+
+  function createSwipeImg(el) {
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: el.parentElement,
+        start: "top 90%",
+        end: "top 90%"
+      }
+    });
+    let tl2 = gsap.timeline({
+      scrollTrigger: {
+        trigger: el.parentElement,
+        start: "top 90%",
+        end: "top 90%"
+      }
+    });
+    tl.fromTo(
+      el.parentElement,
+      {
+        clipPath: "polygon(10% 10%, 90% 10%, 90% 90%, 10% 90%)"
+      },
+      {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        duration: 2,
+        ease: "power3.out"
+      }
+    );
+    tl2.fromTo(
+      el,
+      {
+        scale: 1.5
+      },
+      {
+        scale: 1.2,
+        duration: 2,
+        ease: "power3.out"
+      }
+    );
+  }
+});
