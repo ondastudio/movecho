1310d646c4aaa03c8acb5c9b5add920ebb653f03
Onda <75018767+ondastudio@users.noreply.github.com>
2023-12-04 18:25:53 +0000

    Update and rename images-enter.js to general-images-enter.js

--- general-images-enter.js
+++ general-images-enter.js
@@ -15,37 +15,37 @@ window.addEventListener("load", () => {
       scrollTrigger: {
         trigger: el.parentElement,
         start: "top 90%",
-        end: "top 90%"
-      }
+        end: "top 90%",
+      },
     });
     let tl2 = gsap.timeline({
       scrollTrigger: {
         trigger: el.parentElement,
         start: "top 90%",
-        end: "top 90%"
-      }
+        end: "top 90%",
+      },
     });
     tl.fromTo(
       el.parentElement,
       {
-        clipPath: "polygon(10% 10%, 90% 10%, 90% 90%, 10% 90%)"
+        clipPath: "polygon(10% 10%, 90% 10%, 90% 90%, 10% 90%)",
       },
       {
         clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
         duration: 2,
-        ease: "power3.out"
-      }
+        ease: "power3.out",
+      },
     );
     tl2.fromTo(
       el,
       {
-        scale: 1.5
+        scale: 1.5,
       },
       {
         scale: 1,
         duration: 2,
-        ease: "power3.out"
-      }
+        ease: "power3.out",
+      },
     );
   }
 
@@ -54,37 +54,37 @@ window.addEventListener("load", () => {
       scrollTrigger: {
         trigger: el.parentElement,
         start: "top 90%",
-        end: "top 90%"
-      }
+        end: "top 90%",
+      },
     });
     let tl2 = gsap.timeline({
       scrollTrigger: {
         trigger: el.parentElement,
         start: "top 90%",
-        end: "top 90%"
-      }
+        end: "top 90%",
+      },
     });
     tl.fromTo(
       el.parentElement,
       {
-        clipPath: "polygon(10% 10%, 90% 10%, 90% 90%, 10% 90%)"
+        clipPath: "polygon(10% 10%, 90% 10%, 90% 90%, 10% 90%)",
       },
       {
         clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
         duration: 2,
-        ease: "power3.out"
-      }
+        ease: "power3.out",
+      },
     );
     tl2.fromTo(
       el,
       {
-        scale: 1.5
+        scale: 1.5,
       },
       {
         scale: 1.2,
         duration: 2,
-        ease: "power3.out"
-      }
+        ease: "power3.out",
+      },
     );
   }
 });
