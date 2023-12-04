9f40ce84ecca8992e9fb7a7f818a6cd9160000f5
Onda <75018767+ondastudio@users.noreply.github.com>
2023-12-04 18:23:29 +0000

    Update and rename fix-labels.js to general-fix-labels.js

--- general-fix-labels.js
+++ general-fix-labels.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", () => {
+function fixLabels() {
   const names = document.querySelectorAll(".home_proj-name_wrap");
 
   names.forEach((name) => {
@@ -10,11 +10,11 @@ window.addEventListener("load", () => {
     const width = Math.ceil(text.getBoundingClientRect().width);
     return width;
   }
-  
+
   window.addEventListener("resize", () => {
     names.forEach((name) => {
       name.style.width = null;
       name.style.width = getTextWidth(name.querySelector("p")) + "px";
     });
   });
-});
+}
