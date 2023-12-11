1b1344e0f439ae41e4b7c5c499d6538d82338c87
Onda <75018767+ondastudio@users.noreply.github.com>
2023-12-11 11:40:56 +0000

    Update process-side-links.js

--- process/process-side-links.js
+++ process/process-side-links.js
@@ -1,4 +1,5 @@
 function processSideLinks() {
+  // DESKTOP
   const sideLinks = document.querySelectorAll(".processo-link");
   const bullet = document.querySelector(".process-bullet");
   const inside = [];
@@ -38,4 +39,46 @@ function processSideLinks() {
     }
     if (none) bullet.style.opacity = 0;
   });
+
+  // MOBILE
+  if (window.innerWidth <= 991) {
+    const dropdown = document.querySelector(".processo-dropdown");
+    const dropLinks = dropdown.querySelectorAll(".dropdown-tab-link");
+    const dropBullet = dropdown.querySelector(".process-bullet-mobile");
+    const dropArrow = dropdown.querySelector(".dropdown-arrow");
+    let dropHeight = dropdown.getBoundingClientRect().height;
+    dropdown.style.height = "auto";
+    let openDropHeight = dropdown.getBoundingClientRect().height;
+    dropdown.style.height = "2rem";
+    let open = false;
+
+    dropLinks.forEach((link, i) => {
+      link.addEventListener("click", (e) => {
+        link.querySelector(".process-dropdown-link").appendChild(dropArrow);
+        link.insertBefore(dropBullet, link.firstChild);
+        if (open) {
+          setTimeout(() => {
+            const top = dropdown.getBoundingClientRect().top;
+            const bottom = link.getBoundingClientRect().bottom;
+            dropHeight = bottom - top;
+            gsap.to(dropdown, {
+              height: dropHeight,
+              duration: 1,
+              ease: "power3.out",
+            });
+            open = false;
+          }, 10);
+        } else {
+          gsap.to(dropdown, {
+            height: openDropHeight,
+            duration: 1,
+            ease: "power3.out",
+          });
+          open = true;
+        }
+      });
+    });
+
+    function changeSection(i) {}
+  }
 }
