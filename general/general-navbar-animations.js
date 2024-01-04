38c64ae754f08460a2304b555b1c0dfe8e93d7ee
Onda <75018767+ondastudio@users.noreply.github.com>
2024-01-04 17:59:50 +0000

    Update general-navbar-animations.js

--- general/general-navbar-animations.js
+++ general/general-navbar-animations.js
@@ -2,8 +2,38 @@ window.addEventListener("load", () => {
   const navbar = document.querySelector(".nav_menu-items-wrap");
   const dropdowns = navbar.querySelectorAll(".nav-dropdown");
 
-  const items = document.querySelectorAll(".nav_menu-item");
+  const items = navbar.querySelectorAll(".nav_menu-item");
   const mainCol = items[0].getAttribute("content-color");
+
+  let isHome = true;
+  let current;
+
+  items.forEach((item, i) => {
+    const links = item.querySelectorAll("a");
+    links.forEach((link) => {
+      if (link.classList.contains("w--current")) {
+        isHome = false;
+        current = i;
+      }
+    });
+  });
+
+  if (!isHome) {
+    items.forEach((item) => {
+      const links = item.querySelectorAll("a");
+      let shouldSkip = false;
+      links.forEach((link) => {
+        if (shouldSkip) return;
+        if (link.classList.contains("w--current")) {
+          item.style.color = "var(--" + mainCol + ")";
+          shouldSkip = true;
+          return;
+        } else {
+          item.style.color = "#757370";
+        }
+      });
+    });
+  }
 
   const menuIcon = document
     .querySelector(".mobile-menu-trigger")
@@ -15,21 +45,36 @@ window.addEventListener("load", () => {
       else lottie.classList.add("is-hidden");
     });
 
-  items.forEach((it) => {
+  items.forEach((it, i) => {
     it.addEventListener("mouseenter", () => {
-      items.forEach((jt) => {
-        if (jt.classList.contains("is-language"))
-          jt.style.color = "var(--" + mainCol + ")"; //1
-        if (jt !== it) {
-          jt.style.color = "#757370"; //0.45
-        }
-      });
+      if (isHome) {
+        items.forEach((jt) => {
+          if (jt.classList.contains("is-language"))
+            jt.style.color = "var(--" + mainCol + ")"; //1
+          if (jt !== it) {
+            jt.style.color = "#757370"; //0.45
+          }
+        });
+      } else {
+        items.forEach((jt, j) => {
+          if (jt === it) jt.style.color = "var(--" + mainCol + ")";
+          if (jt !== it && j !== current) {
+            jt.style.color = "#757370"; //0.45
+          }
+        });
+      }
     });
     it.addEventListener("mouseleave", () => {
-      items.forEach((jt) => {
-        if (jt !== it) jt.style.color = "var(--" + mainCol + ")"; //1;
-        if (jt.classList.contains("is-language")) jt.style.color = "#757370"; //0.45;
-      });
+      if (isHome) {
+        items.forEach((jt) => {
+          if (jt !== it) jt.style.color = "var(--" + mainCol + ")"; //1;
+          if (jt.classList.contains("is-language")) jt.style.color = "#757370"; //0.45;
+        });
+      } else {
+        items.forEach((jt, j) => {
+          if (j !== current) jt.style.color = "#757370"; //0.45;
+        });
+      }
     });
   });
 
@@ -91,5 +136,84 @@ window.addEventListener("load", () => {
         });
       }
     });
+  }
+
+  // NAV COLOR CHANGE
+  const navProp =
+    document.querySelector(".navbar").getBoundingClientRect().height /
+    window.innerHeight;
+  const navBG = document.querySelector(".nav-bg");
+  const navDrop = document.querySelectorAll(".nav-drop-list");
+  const navMobile = document.querySelector(".mobile-menu");
+
+  const light0 = document.querySelectorAll(".background-color-light-0");
+  const l0inside = [];
+  light0.forEach(() => {
+    l0inside.push({ v: false });
+  });
+  const light1 = document.querySelectorAll(".background-color-light-1");
+  const l1inside = [];
+  light1.forEach(() => {
+    l1inside.push({ v: false });
+  });
+  const light2 = document.querySelectorAll(".background-color-light-2");
+  const l2inside = [];
+  light2.forEach(() => {
+    l2inside.push({ v: false });
+  });
+  const light3 = document.querySelectorAll(".background-color-light-3");
+  const l3inside = [];
+  light3.forEach(() => {
+    l3inside.push({ v: false });
+  });
+  const light4 = document.querySelectorAll(".background-color-light-4");
+  const l4inside = [];
+  light4.forEach(() => {
+    l4inside.push({ v: false });
+  });
+  const dark1 = document.querySelectorAll(".background-color-dark-1");
+  const d1inside = [];
+  dark1.forEach(() => {
+    d1inside.push({ v: false });
+  });
+
+  window.addEventListener("scroll", () => {
+    light0.forEach((l0, i) => {
+      changeColor(l0, l0inside[i], "light-0");
+    });
+    light1.forEach((l1, i) => {
+      changeColor(l1, l1inside[i], "light-1");
+    });
+    light2.forEach((l2, i) => {
+      changeColor(l2, l2inside[i], "light-2");
+    });
+    light3.forEach((l3, i) => {
+      changeColor(l3, l3inside[i], "light-3");
+    });
+    light4.forEach((l4, i) => {
+      changeColor(l4, l4inside[i], "light-4");
+    });
+    dark1.forEach((d1, i) => {
+      changeColor(d1, d1inside[i], "dark-1");
+    });
+  });
+
+  function changeColor(section, inside, info) {
+    if (
+      ScrollTrigger.positionInViewport(section, "top") <= navProp &&
+      ScrollTrigger.positionInViewport(section, "bottom") > navProp &&
+      !inside.v
+    ) {
+      navBG.setAttribute("background-color", info);
+      navDrop.forEach((drop) => drop.setAttribute("background-color", info));
+      navMobile.setAttribute("background-color", info);
+      inside.v = true;
+    } else if (
+      (ScrollTrigger.positionInViewport(section, "bottom") <= navProp &&
+        inside.v) ||
+      (ScrollTrigger.positionInViewport(section, "top") > navProp && inside.v)
+    ) {
+      inside.v = false;
+    }
   }
 });
