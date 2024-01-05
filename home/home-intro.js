94f9924a51f6d4f69be7bd033dfeee90a120b5a5
Onda <75018767+ondastudio@users.noreply.github.com>
2024-01-05 15:46:44 +0000

    Update home-intro.js

--- home/home-intro.js
+++ home/home-intro.js
@@ -22,23 +22,24 @@ function homeIntro() {
   });
 
   const tl2 = new gsap.timeline();
-  tl2.to(
-    introChildren,
-    {
-      delay: 1,
-      opacity: 1,
-      duration: 0,
-      stagger: { each: 0.3 },
-      onStart: () => {
-        setTimeout(() => {
-          introVid.play();
-          flipVideo();
-          introAnimation();
-        }, 1200);
-      },
-    },
-    ">",
-  );
+  tl2.to(introChildren, {
+    delay: 1,
+    opacity: 1,
+    duration: 0,
+    stagger: { each: 0.3 },
+    repeat: -1,
+  });
+
+  window.addEventListener("load", () => {
+    tl2.delay(0);
+    tl2.restart();
+    setTimeout(() => {
+      tl2.progress(1).kill();
+      introVid.play();
+      flipVideo();
+      introAnimation();
+    }, 2450);
+  });
 
   function flipVideo() {
     const state = Flip.getState(introVisual);
@@ -56,13 +57,13 @@ function homeIntro() {
   heroIntro.to(
     ".navbar",
     {
-      delay: 1.6,
+      delay: 1.2,
       opacity: 1,
       y: "0%",
       duration: 1.5,
       ease: "power3.out",
     },
-    "<",
+    "<"
   );
 
   function introAnimation() {
