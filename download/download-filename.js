8f8bc6bb8ff68c386623ff68caa4691650c789d0
ondastudio <hello@ondastudio.co>
2026-05-27 12:10:00 +0100

    remove videos hosted on cloudinary + add download filename

---
+++ download/download-filename.js
@@ -0,0 +1,14 @@
+function downloadFilename() {
+  document.querySelectorAll("[display-filename]").forEach((el) => {
+    el.textContent = extractFilename(el.getAttribute("href"));
+  });
+}
+
+function extractFilename(url) {
+  const path = new URL(url).pathname;
+  const lastUnderscoreIndex = path.lastIndexOf("_");
+  const filename = path.slice(lastUnderscoreIndex + 1);
+  // Opcional: remover a extensão
+  // filename = filename.replace(/\.[^/.]+$/, "");
+  return decodeURIComponent(filename);
+}
